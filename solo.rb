d23a6efc0aa2c6a305a036d8bd1a942886ffc85d
Chris Rust <crust4@gmail.com>
2017-07-30 21:42:09 -0500

    saving dummy commands for later

--- solo.rb
+++ solo.rb
@@ -1,3 +1,8 @@
+#root = File.absolute_path(File.dirname(__FILE__))
+#file_cache_path root
+#cookbook_path root + '/cookbooks'
+#role_path root + '/roles'
+
 cookbook_path '/root/chef/cookbooks'
 role_path '/root/chef/roles'
 data_bag_path '/root/chef/data_bags'
